f26c2ce967d092847d4559e357357befa1269336
Greg <gregschoenberg@gmail.com>
2022-04-05 01:39:23 +0000

    build with new pages, changed text on about page because of the warning about unescaped apostrophes

--- pages/about.jsx
+++ pages/about.jsx
@@ -2,7 +2,12 @@ import Link from 'next/link'
 import styles from '../styles/about.module.css'
 
 export default function aboutPage() {
-	
+	const aboutText = `Welcome to a whole new world of opportunity! Join us as we explore the 											amazing world of computer science. Whether you're just getting started in
+					web development, or you're 35 years into your data science career, we 
+					can collaborate and grow. Please explore our blog, check out the photo 
+					and video galleries, and take a peek at our all-new merch section. Contact
+					us below with any requests, suggestions, or proposals. We can't wait to hear
+					from you!`
 	return (
 		<>
 		  <main>
@@ -13,14 +18,7 @@ export default function aboutPage() {
 				  <img className={styles.aboutContentImage} src="https://images.unsplash.com/photo-1600880292089-90a7e086ee0c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="join hands"></img>
 				</div>
 				
-				<p className={styles.aboutContentText}>Welcome to a whole new world of opportunity! Join us as we explore the
-					amazing world of computer science. Whether you're just getting started in
-					web development, or you're 35 years into your data science career, we 
-					can collaborate and grow. Please explore our blog, check out the photo 
-					and video galleries, and take a peek at our all-new merch section. Contact
-					us below with any requests, suggestions, or proposals. We can't wait to hear
-					from you!
-				</p>
+				<p className={styles.aboutContentText}>{aboutText}</p>
 			</section>
 			<Link href='/'>
 			  <a className="btn btn-back">Go Back</a>
